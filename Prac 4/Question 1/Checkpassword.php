413cd8a0668c8aae20472693643fb600e951ae1a
g16t5351 <g16t5351@HUG60>
2019-10-08 16:56:47 +0200

    Prac 4 question 1

---
+++ Prac 4/Question 1/Checkpassword.php
@@ -0,0 +1,22 @@
+<?php
+
+$UserN = $_POST["User_Name"];
+$Pin = $_POST["Password"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+</head>
+<body>
+<h1> Hello 
+
+<?php
+echo $UserN;
+?>
+
+</h1>
+</body>
+</html>
